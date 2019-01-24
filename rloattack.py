240448371d4a0324591250b0e247bf2aa3e03d89
void <6258960+ctrlaltdev@users.noreply.github.com>
2019-01-24 09:04:39 -0800

    Update rloattack.py

--- rloattack.py
+++ rloattack.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 filename = 'legit-img_sjvl\u202Egpj.sh'
 with open(filename, 'w') as f:
-    f.write('#!/usr/bin/bash\necho "ALL YOUR BASE BELONG TO US"')
+    f.write('#!/usr/bin/bash\necho "ALL YOUR BASE ARE BELONG TO US"')
